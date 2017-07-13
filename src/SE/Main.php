1669370c4931cc409a5a778bf149137e061aaf3e
GroupPluginer <4gamesl7o@gmail.com>
2017-07-13 16:00:20 +0300

    Update Main.php

--- src/SE/Main.php
+++ src/SE/Main.php
@@ -20,4 +20,26 @@ class Main extends PluginBase{
      public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
        switch ($cmd->getName()){
         case Super:
-         $
+         $Effect = Effect::getEffect(1);
+         $effect->setAmplifier(3);
+         $effect->setVisable(true);
+         $effect->setDuration(100000000);
+         $sender->addEffect($effect);
+          break;
+        case Super2:
+         $Effect = Effect::getEffect(5);
+         $effect->setAmplifier(2);
+         $effect->setVisable(true);
+         $effect->setDuration(1000000000);
+         $sender->addEffect($effect);
+          break;
+        case Super3:
+         $Effect = Effect::getEffect(10);
+         $effect->setAmplifier(2);
+         $effect->setVisable(true);
+         $effect->setDuratiom(100000000);
+         $sender->addEffect($effect);
+      
+       }
+   }
+}
